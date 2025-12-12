f13b36ce441d6234517a1dcea866ea2ef5a5fc24
unknown <Fayaj Hossain>
2025-12-12 12:02:04 +0530

    Initail commit

---
+++ src/main/java/com/hostpalace/infrasight/InfrasightApplication.java
@@ -0,0 +1,13 @@
+package com.hostpalace.infrasight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrasightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfrasightApplication.class, args);
+	}
+
+}
